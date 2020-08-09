13b693e465cf8c4dd683677e4bfea682ee1df7ac
Eugene Husnullin <eugenehusnullin@gmail.com>
2020-08-09 11:37:37 +0300

    face the problem with overlay

--- src/Modal.js
+++ src/Modal.js
@@ -1,12 +1,35 @@
 import React from 'react'
 
+const MODAL_STYLES = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: 'white',
+  padding: '50px',
+  zIndex: 1000
+}
+
+const OVERLAY_STYLES = {
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  backgroundColor: 'rgba(0, 0, 0, .7)',
+  zIndex: 1000
+}
+
 export default function Modal({ open, onClose, children }) {
   if (!open) return null
 
   return (
-    <div>
-      <button onClick={onClose}>Close Modal</button>
-      {children}
-    </div>
+    <>
+      <div style={OVERLAY_STYLES}></div>
+      <div style={MODAL_STYLES}>
+        <button onClick={onClose}>Close Modal</button>
+        {children}
+      </div>
+    </>
   )
 }
